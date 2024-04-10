b1781436d688de14d5ddc7d97d7d1269f8c51623
Tatyane Gonçalves <158174190+tatyanepgoncalves@users.noreply.github.com>
2024-04-10 11:06:16 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,32 +1,60 @@
 const btnDimiss = document.getElementById("dimiss-btn");
 const btnSubs = document.getElementById("btn-subs");
+
 const card = document.getElementById('card');
 const cardModal = document.getElementById("card-modal");
 const inputEmail = document.getElementById("input-email")
 const erroWarm = document.getElementById("erro-warm");
+const signedEmail = document.getElementById('signed-email');
+const form = document.getElementById('form');
 
-// mensagem de erro 
-inputEmail.addEventListener("input", function(event) {
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+})
 
-    if(inputEmail === ""){
-        erroWarm.style.display = "block";
-        inputEmail.style.border = "hsl(4, 100%, 67%)";
+// Validação de formulário
+function checkInput(){
+
+    if (inputEmail.value === ''){
+        erroWarm.innerText = "Valid email required"
+        erroWarm.style.display = 'block'
+
+    } else if (!inputEmail.value.includes('@')){
+        erroWarm.innerText = "Email is invalid"
+        erroWarm.style.display = 'block'
+        
+    } else if (inputEmail.value.length < 3){
+        erroWarm.innerText = "Email is too short"
+        erroWarm.style.display = 'block'
+
+    } else {
+        showModal()
     }
-});
+}
+
+// 
+btnSubs.addEventListener('click', checkInput)
 
 
-
-
-
+// ocultar o modal
+btnDimiss.addEventListener('click', hiddenModal)
 
 // mostrar card modal 
 function showModal() {
+    signedEmail.innerText = inputEmail.value
+
     card.style.display = 'none';
     cardModal.style.display = 'flex';
 }
   
 // ocultar modal 
 function hiddenModal(){
-    cardModal.style.display = "hidden";
-    card.style.display = "flex";
+    cardModal.style.display = "none";
+    card.style.display = "flex";  
+    clearInput() 
+}
+
+// limpar input
+function clearInput(){
+    inputEmail.value = ''
 }
